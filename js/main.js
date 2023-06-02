8ebc425d54c72c5e7c31aebd4bac9bfcc1b1e38a
D.Williams <302477878@student.rocmondriaan.nl>
2023-06-02 09:53:52 +0200

    Added all the pages & small JS changes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const goalContainer = document.querySelector('.goal-container');
 const sourceContainer = document.querySelector('.source-container');
 const finishedContainer = document.querySelector('.finished-container');
 const swmgImage = document.querySelector('.swmg-img');
+let currentWindow = window.location.href;
 
 const goals = [
     {
@@ -24,23 +25,24 @@ function addGoal(){
 
 for (let i = 0; i < goals.length; i++) {
     const goal = goals[i];
-    console.log(goal.learninGoal);
 }
 
 let learningGoals = ["Gradient borders", "Minor CSS Animations", "Append Children"];
 let Sources = ["https://css-tricks.com/gradient-borders-in-css/", "https://animate.style/", "https://www.w3schools.com/jsref/met_node_appendchild.asp"];
 let Finished = [false, false, false];
+if(currentWindow.includes("sequencePlan")){
 
-for (let i = 0; i < learningGoals.length; i++) {
-    const learningGoalsArray = learningGoals[i];
-    const sourcesArray = Sources[i];
-    const finishedArray = Finished[i];
-    numberContainer.innerHTML += ` <li class="list-group-item list-number ms-2 w-5 mb-1">${i + 1}</li>`
-    goalContainer.innerHTML += `<li class="list-group-item list-goal ms-2 w-10 mb-1">${learningGoalsArray}</li>`
-    sourceContainer.innerHTML += `<li class="list-group-item list-source ms-2 w-10 mb-1">${sourcesArray}</li>`
-    finishedContainer.innerHTML += `<li class="list-group-item border-bottom list-finished ms-2 w-5 mb-1 text-danger">${finishedArray}</li>`
+    for (let i = 0; i < learningGoals.length; i++) {
+        const learningGoalsArray = learningGoals[i];
+        const sourcesArray = Sources[i];
+        const finishedArray = Finished[i];
+        numberContainer.innerHTML += ` <li class="list-group-item list-number ms-2 w-5 mb-1">${i + 1}</li>`
+        goalContainer.innerHTML += `<li class="list-group-item list-goal ms-2 w-10 mb-1">${learningGoalsArray}</li>`
+        sourceContainer.innerHTML += `<li class="list-group-item list-source ms-2 w-10 mb-1">${sourcesArray}</li>`
+        finishedContainer.innerHTML += `<li class="list-group-item border-bottom list-finished ms-2 w-5 mb-1 text-danger">${finishedArray}</li>`
+    }
 }
-
+    
 // swmgImage.addEventListener('mouseleave', function(){
 //     swmgImage.style.visibility = "hidden";
 //     console.log("Element is invisible");
