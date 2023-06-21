943c8f16cc0a9e7a189bb20569a603ea74ab9fd0
D.Williams <302477878@student.rocmondriaan.nl>
2023-06-21 11:47:43 +0200

    Added more content & finished linking to projects

---
+++ projects/boter_kaas_eieren/js/main.js
@@ -0,0 +1,36 @@
+const tile1 = document.querySelector(".content1");
+const tile2 = document.querySelector(".content2");
+const tile3 = document.querySelector(".content3");
+const tile4 = document.querySelector(".content4");
+const tile5 = document.querySelector(".content5");
+const tile6 = document.querySelector(".content6");
+const tile7 = document.querySelector(".content7");
+const tile8 = document.querySelector(".content8");
+const tile9 = document.querySelector(".content9");
+const play = document.querySelector(".play");
+const grid = document.querySelectorAll(".content1, .content2, .content3, .content4, .content5, .content6, .content7, .content8, .content9");
+const Leaderboard = document.querySelector(".leaderboard");
+const username = document.querySelector(".username");
+
+
+play.addEventListener("click", function(){
+
+});
+// tile1.addEventListener("click", );
+// tile2.addEventListener("click", );
+// tile3.addEventListener("click", );
+// tile4.addEventListener("click", );
+// tile5.addEventListener("click", );
+// tile6.addEventListener("click", );
+// tile7.addEventListener("click", );
+// tile8.addEventListener("click", );
+// tile9.addEventListener("click", );
+Leaderboard.addEventListener("click", highscore);
+
+function highscore(){
+    
+}
+
+function backgroundSwitch(){
+    
+}
